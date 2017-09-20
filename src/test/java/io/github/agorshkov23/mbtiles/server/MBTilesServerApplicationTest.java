7fd2670df1d288ad29e54f594b9b22d016a544e0
Alexey Gorshkov <agorshkov23@mail.ru>
2017-09-20 12:54:00 +0300

    added test application with test .mbtiles file and test configuration

---
+++ src/test/java/io/github/agorshkov23/mbtiles/server/MBTilesServerApplicationTest.java
@@ -0,0 +1,10 @@
+package io.github.agorshkov23.mbtiles.server;
+
+import org.springframework.boot.SpringApplication;
+
+public class MBTilesServerApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MBTilesServerApplication.class, args);
+    }
+}
